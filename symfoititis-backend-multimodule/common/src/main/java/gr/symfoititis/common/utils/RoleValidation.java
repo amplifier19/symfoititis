6d773dbea057e2e4986e71bb6688dd3deb3d980d
amplifier19 <dimitris.leontaridis@yahoo.gr>
2024-10-09 13:39:33 +0300

    Add role validation functionality, enforcing RBAC across services

--- symfoititis-backend-multimodule/common/src/main/java/gr/symfoititis/common/utils/RoleValidation.java
+++ symfoititis-backend-multimodule/common/src/main/java/gr/symfoititis/common/utils/RoleValidation.java
@@ -1,2 +1,23 @@
-package gr.symfoititis.common.utils;public class RoleValidation {
+package gr.symfoititis.common.utils;
+
+import gr.symfoititis.common.exceptions.ForbiddenException;
+
+public class RoleValidation {
+    public enum Role {
+        student,
+        teacher,
+        admin
+    }
+    public static void isAdmin(String role) {
+        if (Role.admin.toString().equals(role)) return;
+        throw new ForbiddenException("Forbidden");
+    }
+    public static void isStudent(String role) {
+        if (Role.student.toString().equals(role)) return;
+        throw new ForbiddenException("Forbidden");
+    }
+    public static void isTeacher(String role) {
+        if (Role.teacher.toString().equals(role)) return;
+        throw new ForbiddenException("Forbidden");
+    }
 }

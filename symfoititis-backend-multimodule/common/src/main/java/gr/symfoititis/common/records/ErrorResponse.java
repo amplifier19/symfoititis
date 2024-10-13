721acbe7ff51a1ddac263bfb4576e503fdad9851
amplifier19 <dimitris.leontaridis@yahoo.gr>
2024-10-13 19:37:31 +0300

    Use UtcTimestamp utility from common module

--- symfoititis-backend-multimodule/common/src/main/java/gr/symfoititis/common/records/ErrorResponse.java
+++ symfoititis-backend-multimodule/common/src/main/java/gr/symfoititis/common/records/ErrorResponse.java
@@ -1,2 +1,9 @@
-package gr.symfoititis.common.records;public record ErrorResponse() {
+package gr.symfoititis.common.records;
+
+import static gr.symfoititis.common.utils.UtcTimestamp.getTimestamp;
+
+public record ErrorResponse(Integer status, String timestamp, String error, String path) {
+    public ErrorResponse(Integer status, String error, String path) {
+        this(status, getTimestamp(), error, path);
+    }
 }
--- symfoititis-backend-multimodule/common/src/main/java/gr/symfoititis/common/records/Response.java
+++ symfoititis-backend-multimodule/common/src/main/java/gr/symfoititis/common/records/Response.java
@@ -1,18 +1,9 @@
 package gr.symfoititis.common.records;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
+import static gr.symfoititis.common.utils.UtcTimestamp.getTimestamp;
 
 public record  Response (Integer status, String timestamp, Object data) {
     public Response (Integer status, Object data) {
         this(status, getTimestamp(), data);
-    }
-
-    private static String getTimestamp () {
-        Date now = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-        sdf.setTimeZone(TimeZone.getTimeZone("UTC+0"));
-        return sdf.format(now);
     }
 }
